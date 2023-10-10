bad3c69d9c051c1098b78404d51fbdad777584b9
hanna0115 <hanna0115@naver.com>
2023-10-10 13:17:48 +0900

    ✨ HomePage 컴포넌트 추가

--- chorogi/src/App.js
+++ chorogi/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import HomePage from './component/HomePage';
 import Nav from './component/Nav';
+import HomePage from './component/HomePage';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Nav/>
         <HomePage/>
       </div>
     </BrowserRouter>
