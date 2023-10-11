cd9f94c43af11253a502894f6a33c1cad178ff3e
Ji2unKo <wldnjs4983@naver.com>
2023-10-11 13:53:33 +0900

    📝 sprout mark up

--- chorogi/src/component/Nav.jsx
+++ chorogi/src/component/Nav.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
-import logo from './logo.jpg';
 
 const Nav = () => {
   const menuItems = document.querySelectorAll('.gnb > li');
@@ -11,13 +10,11 @@ const Nav = () => {
   
     item.addEventListener('mouseover', function(){
       subMenu.classList.add('active');
-      this.querySelector('a').classList.add('on');
     })
   
     // mouseleave
     item.addEventListener('mouseleave', function(){
       subMenu.classList.remove('active');
-      this.querySelector('a').classList.remove('on');
     })
   })
   return (
@@ -25,13 +22,13 @@ const Nav = () => {
       <nav>
         <h1 className="logo">
           <Link to href="">
-            <img src="/images/logo_black.png" alt="logoimg" />
+            <img src="/images/logo_black.png" alt="logo-img" />
           </Link>
         </h1>
-        <h1 class="visually-hidden">메뉴</h1>
+        <h1 className="visually-hidden">메뉴</h1>
         <ul className="gnb">
-          <li className='sub-item'><img src="/images/sprout.png" alt=""/>
-          <Link to=''>탄소중립이란?</Link>
+          <li><Link to=''>탄소중립이란?</Link>
+            <img className='sprout' src="/images/sprout.png" alt="" />
             <ul className="sub">
               <li><Link to=''>탄소중립이란</Link></li>
               <li><Link to=''>탄소중립 생활실천</Link></li>
@@ -39,12 +36,14 @@ const Nav = () => {
             </ul>
           </li>
           <li><Link to=''>수거함 위치 알리미</Link>
+          <img className='sprout' src="/images/sprout.png" alt="" />
             <ul className="sub">
               <li><Link to=''>의류수거함</Link></li>
               <li><Link to=''>폐건전지, 형광등</Link></li>
             </ul>
           </li>
           <li><Link to=''>지구 살리기 Tip!</Link>
+          <img className='sprout' src="/images/sprout.png" alt="" />
             <ul className="sub">
               <li><Link to=''>분리수거하고 돈벌기</Link></li>
               <li><Link to=''>가까운 곳에 기부하기</Link></li>
