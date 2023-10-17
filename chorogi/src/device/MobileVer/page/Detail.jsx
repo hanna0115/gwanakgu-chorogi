6858ad9edb612049c00fede9f4f7ee09bfcd2883
hanna0115 <hanna0115@naver.com>
2023-10-17 16:44:39 +0900

    ✨ 파라미터에 따라 상세 페이지 정보 바뀌도록 구현

--- chorogi/src/device/MobileVer/page/Detail.jsx
+++ chorogi/src/device/MobileVer/page/Detail.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import './Detail.css';
 import { Icon } from '@iconify/react';
 import { collectionBoxDetailData } from '../component/CollectionBoxData';
@@ -9,46 +9,44 @@ const Detail = () => {
   const { detailSlug } = useParams();
 
   const getContents = (detailSlug) => {
-    return collectionBoxDetailData.find((content) => content.slug === detailSlug);
+    return collectionBoxDetailData.find((content) => content.detailSlug === detailSlug);
   }
 
   const contents = getContents(detailSlug);
-  console.log(contents)
+  
+  const navigate = useNavigate();
+  const onClickBtn = () => {
+    navigate(-1);
+  };
+  
   return (
     <div>
       <div className="map-title-box">
         <div className="map-title">
-          <p>수거 위치 알리미</p>
-          <h3>의류 수거함</h3>
+          <p>{contents.menu}</p>
+          <h3>{contents.title}</h3>
         </div>
-        <div className="map-title-btn">
-          <Link to="/"><Icon icon="uiw:left" /></Link>
+        <div className="map-title-btn" onClick={onClickBtn}>
+          <Link><Icon icon="uiw:left" /></Link>
         </div>
       </div>
-      {
-        collectionBoxDetailData.map((item, index) => {
-          console.log(item)
-          return (
-            <div class="box-list">
-              <div class="box-item">
-                <img src={item.mainImg} alt={item.title} />
-                <h2>{item.mainTitle}</h2>
-                <p>{item.mainContent}</p>
-              </div>
-              <div class="box-item">
-                <Icon icon="ic:outline-circle" width="80px" color="5B92FB"/>
-                <span>{item.subTitle1}</span>
-                <p>{item.subContent1}</p>
-              </div>
-              <div class="box-item">
-                <Icon icon="ph:x-bold" width="80px" color="FF1212"/>
-                <span>{item.subTitle2}</span>
-                <p>{item.subContent2}</p>
-              </div>
-            </div>
-          )
-        })
-      }
+      <div class="box-list">
+        <div class="box-item">
+          <img src={contents.mainImg} alt={contents.title} />
+          <h2>{contents.mainTitle}</h2>
+          <p>{contents.mainContent}</p>
+        </div>
+        <div class="box-item">
+          <Icon icon="ic:outline-circle" width="80px" color="5B92FB"/>
+          <span>{contents.subTitle1}</span>
+          <p>{contents.subContent1}</p>
+        </div>
+        <div class="box-item">
+          <Icon icon="ph:x-bold" width="80px" color="FF1212"/>
+          <span>{contents.subTitle2}</span>
+          <p>{contents.subContent2}</p>
+        </div>
+      </div>
     </div>
   );
 };
