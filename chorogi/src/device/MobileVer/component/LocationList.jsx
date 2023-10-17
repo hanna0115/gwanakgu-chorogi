8e5f3611579c9faba2adb85a98ca3bd3e82fac87
hannah <hanna0115@naver.com>
2023-10-18 00:08:32 +0900

    ✨ 클릭한 마커 정보 보이게 기능 구현

--- chorogi/src/device/MobileVer/component/LocationList.jsx
+++ chorogi/src/device/MobileVer/component/LocationList.jsx
@@ -4,10 +4,31 @@ import './LocationList.css';
 import { Icon } from '@iconify/react';
 
 const LocationList = (props) => {
-  const locationList = props.locationList
+  const locationList = props.locationList;
+  const selectedIndex = props.index;
+  const selectedItem = locationList.item[selectedIndex];
 
   return (
     <ul className="location-list">
+      <li className="location-item">
+        <div className="location-info">
+          <img src={locationList.img} alt={locationList.title} />
+          <div>
+            <p>{selectedItem.name} <span>300m</span></p>
+            <p>{selectedItem.address}</p>
+          </div>
+        </div>
+        <div className="location-item-btn">
+          <Link to="">
+            <Icon icon="solar:share-bold-duotone" />
+            <span>공유하기</span>
+          </Link>
+          <Link to={`https://map.kakao.com/link/to/${selectedItem.address},${selectedItem.lat},${selectedItem.lng}`}>
+            <Icon icon="fluent:directions-24-regular" />
+            <span>길찾기</span>
+          </Link>
+        </div>
+      </li>
     {
       locationList.item.map((item, index) => {
         return (
@@ -21,12 +42,12 @@ const LocationList = (props) => {
             </div>
             <div className="location-item-btn">
               <Link to="">
-                  <Icon icon="solar:share-bold-duotone" />
-                  <span>공유하기</span>
+                <Icon icon="solar:share-bold-duotone" />
+                <span>공유하기</span>
               </Link>
-              <Link to="">
-                  <Icon icon="fluent:directions-24-regular" />
-                  <span>길찾기</span>
+              <Link to={`https://map.kakao.com/link/to/${item.address},${item.lat},${item.lng}`}>
+                <Icon icon="fluent:directions-24-regular" />
+                <span>길찾기</span>
               </Link>
             </div>
           </li>
